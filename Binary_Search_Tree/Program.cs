57689cf9ed9e620a375f1ae2251c40d3edab67af
Tushar Anand <tusharranand@gmail.com>
2022-03-29 14:45:43 +0530

    Introduction

---
+++ Binary_Search_Tree/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace Binary_Search_Tree
+{
+    class Program
+    {
+        public static void Main(string[] args)
+        {
+            Console.WriteLine("Welcome to Binary Search Tree Program");
+        }
+    }
+}
