fc7bd7b55b743d98c734461d7dadfb3bf9ca36a6
Tushar Anand <tusharranand@gmail.com>
2022-03-30 15:37:47 +0530

    [Tushar Anand] Modified: Ability to create the given binary search tree

--- Binary_Search_Tree/BST.cs
+++ Binary_Search_Tree/BST.cs
@@ -6,40 +6,84 @@ using System.Threading.Tasks;
 
 namespace Binary_Search_Tree
 {
-    public class BST
+    public class BST<T> where T : IComparable
     {
-        Binary_Node Root;
 
-        public BST(Binary_Node node)
+        Binary_Node<T> Head;
+        Binary_Node<T> temp;
+        public BST(T Key)
         {
-            Root = node;
+            this.Head = new Binary_Node<T>(Key);
         }
-        public void Insert_Default(Binary_Node Parent, Binary_Node node)
+        public void Insert(T Data)
         {
-            if (Parent.Left == null)
+            temp = Head;
+            Insert_R(Data);
+        }
+        public void Insert_R(T Data)
+        {
+            if (temp.Value.CompareTo(Data) > 0 && temp.Left == null)
             {
-                Console.WriteLine("Inserting {0} to the left of root {1}", node.Key, Parent.Key);
-                Parent.Left = node;
-                node.Root = Parent;
+                Binary_Node<T> left = new Binary_Node<T>(Data);
+                temp.Left = left;
             }
-            else
+            else if (temp.Value.CompareTo(Data) > 0 && temp.Left != null)
             {
-                Console.WriteLine("Inserting {0} to the right of root {1}", node.Key, Parent.Key);
-                Parent.Right = node;
-                node.Root = Parent;
+                temp = temp.Left;
+                Insert_R(Data);
+            }
+            else if (temp.Value.CompareTo(Data) < 0 && temp.Right == null)
+            {
+                Binary_Node<T> right = new Binary_Node<T>(Data);
+                temp.Right = right;
+            }
+            else if (temp.Value.CompareTo(Data) < 0 && temp.Right != null)
+            {
+                temp = temp.Right;
+                Insert_R(Data);
             }
         }
-        public void Insert_Left(Binary_Node Parent, Binary_Node node)
+        public void Display_Inorder()
         {
-            Console.WriteLine("Inserting {0} to the left of root {1}", node.Key, Parent.Key);
-            Parent.Left = node;
-            node.Root = Root;
+            Console.WriteLine("Displaying the Binary Search Tree InOrder");
+            Display_Inorder(Head);
+            Console.WriteLine("Binary Search Tree empty nothing to display.\n");
         }
-        public void Insert_Right(Binary_Node Parent, Binary_Node node)
+        public void Display_Inorder(Binary_Node<T> node)
         {
-            Console.WriteLine("Inserting {0} to the right of root {1}", node.Key, Parent.Key);
-            Parent.Right = node;
-            node.Root = Parent;
+            if (node.Left != null)
+                Display_Inorder(node.Left);
+            Console.WriteLine(node.Value);
+            if (node.Right != null)
+                Display_Inorder(node.Right);
+        }
+        public void Display_Preorder()
+        {
+            Console.WriteLine("Displaying the Binary Search Tree PreOrder");
+            Display_Preorder(Head);
+            Console.WriteLine("Binary Search Tree empty nothing to display.\n");
+        }
+        public void Display_Preorder(Binary_Node<T> node)
+        {
+            Console.WriteLine(node.Value);
+            if (node.Left != null)
+                Display_Preorder(node.Left);
+            if (node.Right != null)
+                Display_Preorder(node.Right);
+        }
+        public void Display_Postorder()
+        {
+            Console.WriteLine("Displaying the Binary Search Tree PostOrder");
+            Display_Postorder(Head);
+            Console.WriteLine("Binary Search Tree empty nothing to display.\n");
+        }
+        public void Display_Postorder(Binary_Node<T> node)
+        {
+            if (node.Left != null)
+                Display_Postorder(node.Left);
+            if (node.Right != null)
+                Display_Postorder(node.Right);
+            Console.WriteLine(node.Value);
         }
     }
 }
--- Binary_Search_Tree/Program.cs
+++ Binary_Search_Tree/Program.cs
@@ -8,34 +8,24 @@ namespace Binary_Search_Tree
         {
             Console.WriteLine("Welcome to Binary Search Tree Program");
 
-            Binary_Node node_one = new Binary_Node(56);
-            Binary_Node node_two = new Binary_Node(30);
-            Binary_Node node_three = new Binary_Node(70);
-            Binary_Node node_four = new Binary_Node(22);
-            Binary_Node node_five = new Binary_Node(40);
-            Binary_Node node_six = new Binary_Node(11);
-            Binary_Node node_seven = new Binary_Node(3);
-            Binary_Node node_eight = new Binary_Node(16);
-            Binary_Node node_nine = new Binary_Node(60);
-            Binary_Node node_ten = new Binary_Node(95);
-            Binary_Node node_eleven = new Binary_Node(65);
-            Binary_Node node_twelve = new Binary_Node(63);
-            Binary_Node node_thirteen = new Binary_Node(67);
+            BST<int> Tree = new BST<int>(56);
+            Tree.Insert(30);
+            Tree.Insert(70);
+            Tree.Insert(22);
+            Tree.Insert(40);
+            Tree.Insert(60);
+            Tree.Insert(95);
+            Tree.Insert(11);
+            Tree.Insert(65);
+            Tree.Insert(3);
+            Tree.Insert(16);
+            Tree.Insert(63);
+            Tree.Insert(67);
 
 
-            BST Tree = new BST(node_one);                           //56
-            Tree.Insert_Left(node_one, node_two);                   //30
-            Tree.Insert_Right(node_one, node_three);                //70
-            Tree.Insert_Left(node_two, node_four);                  //22
-            Tree.Insert_Right(node_two, node_five);                 //40
-            Tree.Insert_Left(node_four, node_six);                  //11
-            Tree.Insert_Left(node_six, node_seven);                 //3
-            Tree.Insert_Right(node_six, node_eight);                //16
-            Tree.Insert_Left(node_three, node_nine);                //60
-            Tree.Insert_Right(node_three, node_ten);                //95
-            Tree.Insert_Right(node_nine, node_eleven);              //65
-            Tree.Insert_Left(node_eleven, node_twelve);             //63
-            Tree.Insert_Right(node_eleven, node_thirteen);          //67
+            Tree.Display_Inorder();
+            Tree.Display_Preorder();
+            Tree.Display_Postorder();
         }
     }
 }
